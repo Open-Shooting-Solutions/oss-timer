d59609a040cb2953f236f1494d6f98c0112dbf65
arnoudx6 <arnoud.stolk@ziggo.nl>
2024-08-25 22:52:55 +0200

    Setting up stage timer

--- src/oss-timer/oss/timer/models/timers.py
+++ src/oss-timer/oss/timer/models/timers.py
@@ -1,6 +1,8 @@
 from enum import Enum
 from oss.timer.timers.stage import StageTimer
+from oss.timer.timers.competition import CompetitionTimer
 
 
 class Timer(Enum):
     STAGE: StageTimer = StageTimer
+    COMPETITION: CompetitionTimer = CompetitionTimer
---
+++ src/oss-timer/oss/timer/timers/competition.py
@@ -0,0 +1,18 @@
+# For LG, KKP, ETC etc.
+from oss.core.log import Log
+from oss.core.message import BrokerConnection, BrokerExchangeType
+from oss.timer.models.timer import BaseTimer
+
+# Activate module wide logging
+logger = Log.get_logger_function()(__name__)
+
+
+class CompetitionTimer(BaseTimer):
+    def __init__(self) -> None:
+        pass
+
+    def __del__(self) -> None:
+        pass
+
+    def _handle_broker_message(self, ch, method, properties, body) -> None:
+        pass
